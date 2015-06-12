e5e6f088cf0d0a25beef241f32e447e6b4c6e40f
jarjin <2338894521@qq.com>
2015-06-13 01:42:11 +0800

    添加新手引导面板

--- Assets/Editor/BindLua.cs
+++ Assets/Editor/BindLua.cs
@@ -361,7 +361,7 @@ public static class LuaBinding
     }
 
     [MenuItem("Lua/Clear LuaBinder File + Wrap Files", false, 13)]
-    static void ClearLuaBinder()
+    public static void ClearLuaBinder()
     {
         StringBuilder sb = new StringBuilder();
         sb.AppendLine("using System;");
